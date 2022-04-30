19a13f26a2e263b84745e1ab9f3d2a415fa5e7b6
qrmoon <piotrcz@posteo.net>
2022-05-01 00:21:21 +0200

    Update send wrapper

--- asyncio/TCP.lua
+++ asyncio/TCP.lua
@@ -39,12 +39,40 @@ function TCP:accept()
   return sock, err
 end
 
-for _, f in ipairs {
-  "connect",
-  "receive",
-  "send"
-} do
-  TCP[f] = Socket._wrap_blocking(f)
+-- function TCP:connect(...)
+--   local args = { ... }
+--   if self.timeout == 0 then
+--     return fiber.wait(function()
+--       local ok, err = self.sock:connect(table.unpack(args))
+--       print(ok, err, self.sock)
+--       return ok, err
+--     end)
+--   else
+--   end
+-- end
+
+TCP.connect = Socket._wrap_blocking "connect"
+TCP.receive = Socket._wrap_blocking "receive"
+
+function TCP:send(s, i, j)
+  local i = i or 1
+  if self.timeout == 0 then
+    return fiber.wait(function()
+      local ok, err, index = self.sock:send(s, i, j)
+      if err == "timeout" then
+        i = index + 1
+      end
+      return err ~= "timeout"
+    end)
+  else
+    return fiber.wait_for(self.timeout, function()
+      local ok, err, index = self.sock:send(s, i, j)
+      if err == "timeout" then
+        i = index + 1
+      end
+      return err ~= "timeout"
+    end)
+  end
 end
 
 for _, f in ipairs {
